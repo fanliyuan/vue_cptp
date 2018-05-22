8fc89fcd0095c52f2e2e8c6c5d7a8a9e72a961bc
ChouEric <choueric@foxmail.com>
2018-05-22 09:47:12 +0800

    5-22

--- src/pages/auth/authSearchResult.vue
+++ src/pages/auth/authSearchResult.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    权限搜索结果页
+    <img :src="url" alt="" v-on:error="error">
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      url: '/staticm/img/avatar.png'
+    }
+  },
+  methods: {
+    error () {
+      this.url = 'https://www.baidu.com/img/540-258%20dong_0ca7c638f1d7a2376739fe2d382feef1.gif'
+    }
+  }
+}
+</script>
